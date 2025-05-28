a7932118b81e4f80882fcca9b2e2d89fe519725f
Angel Magana <maganaangel342@gmail.com>
2025-05-28 14:56:06 -0600

    puerto cambiado

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.json()); //Recibir los datos en JSON
 // });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor backend corriendo en el puerto ${PORT}`);
 });
