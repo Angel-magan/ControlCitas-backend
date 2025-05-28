198ba65b8a793bf92c94ca5d09dd6f2e0acf539e
Angel Magana <maganaangel342@gmail.com>
2025-05-28 12:11:38 -0600

    Agregando cambios en el package.json para ejecución

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "control_citas",
+  port: process.env.DB_PORT || 3305,
 });
 
 module.exports = db;
