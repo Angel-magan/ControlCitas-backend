82df803e2a9c607a9290a3b4c3fb01d13eac640f
Angel Magana <maganaangel342@gmail.com>
2025-05-28 13:38:58 -0600

    Puerto diferente

--- config/db.js
+++ config/db.js
@@ -20,7 +20,7 @@ function connectWithRetry() {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 3305,
   });
 
   connection.connect((err) => {
