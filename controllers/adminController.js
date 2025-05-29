21b025edddba18ccc2a8ad7167bb2680a4825e38
Maicol Monge <maicol.monge@catolica.edu.sv>
2025-05-29 01:57:27 -0600

    redireccion de login correo

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs"); // Al inicio del archivo
 const nodemailer = require("nodemailer"); // Agrega esto al inicio del archivo
 const frontendHost = process.env.DB_HOST || "localhost";
 const frontendPort = process.env.PORT || "5173";
-const frontendUrl = `http://${frontendHost}:${frontendPort}/`; //Url para el login del correo
+const frontendUrl = `https://controlcitas-frontend-production.up.railway.app/`; //Url para el login del correo
 
 // Listar todos los usuarios
 exports.getUsuarios = (req, res) => {
